81001f00324fefded72d1d331cd27da9ced56ba5
VadimNikitin1 <vchivas18years@gmail.com>
2023-07-25 12:44:05 +0300

    add register

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 import express from 'express'
 import authRoutes from './app/auth/auth.routes.js'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
 import 'colors'
+import { prisma } from './app/prisma.js'
+import { errorHandler, notFound } from './app/middleware/error.middleware.js'
+
+// dotenv загружает переменные из файла .env в файлы process.env
+dotenv.config()
 
 const app = express()
 
 async function main() {
-	// if (process.env.NODE_ENV === 'development') {
-	// 	app.use(morgan('dev'))
-	// }
+	// morgan для логирования запросов
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'))
+	}
 
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
 
-	const PORT = 4200
+	app.use(notFound)
+	app.use(errorHandler)
+
+	const PORT = process.env.PORT || 4200
 
 	app.listen(
 		PORT,
 		console.log(
-			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+				.bold
 		)
 	)
 }
 
 main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
